268bd692c537f533570d4a3790c07bc417dd2819
Asepimam <asepimammutaqin317@gmail.com>
2023-10-13 20:04:22 +0700

    add chi and modify main route

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/hallo", basicHendler)
+	server := &http.Server{
+		Addr:    ":3000",
+		Handler: r,
+	}
+	erro := server.ListenAndServe()
+	if erro != nil {
+		fmt.Println("failed to listen server", erro)
+	}
+
+}
+func basicHendler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
 }
