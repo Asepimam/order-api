3cfe8ef759fea5b5e809066281b234fcdc1d59ca
Asepimam <asepimammutaqin317@gmail.com>
2023-10-14 06:26:48 +0700

    add aplication package and handel order

--- aplications/routers.go
+++ aplications/routers.go
@@ -3,6 +3,7 @@ package aplications
 import (
 	"net/http"
 
+	"github.com/Asepimam/go-micservic.git/handler"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
@@ -13,5 +14,16 @@ func loadRoutes() *chi.Mux {
 	router.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 	})
+	router.Route("/orders", LoadOrderRouter)
 	return router
+}
+
+func LoadOrderRouter(r chi.Router) {
+	orderHandler := &handler.Order{}
+
+	r.Post("/", orderHandler.Create)
+	r.Get("/", orderHandler.List)
+	r.Get("/{id}", orderHandler.GetByID)
+	r.Put("/{id}", orderHandler.UpdateByID)
+	r.Delete("/{id}", orderHandler.DeleteByID)
 }
